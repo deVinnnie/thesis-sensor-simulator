fb7d14ab44b90e093306dead584ae043f78634a6
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2016-03-23 12:41:19 +0100

    Make transmission failsafe

--- sensor_simulator/models.py
+++ sensor_simulator/models.py
@@ -25,7 +25,9 @@ class Gateway:
 
         globals.scheduler.add_job(self.transmit, 'interval', seconds=self.interval)
         globals.scheduler.add_job(self.do_readings, 'interval', seconds=self.sensorInterval)
-    
+
+        self.readingsFailed = []
+        self.readingsSuccess = []
     
     def add_sensor(self, sensor, save=True):
         sensor.gateway = self
@@ -35,26 +37,92 @@ class Gateway:
         """
         Posts all readings in the queue to the REST API.
 
-        Number of requests = #sensors
+        The measurement data is packed for each sensor.
+        This means that the gateway will send all measurements for a sensor in one request.
+        The total number of requests per transmission for the entire installation is: #gateways x #sensors.
+
+        To ensure that the connection is up the gateway will first try a get request
+        on it's own REST resource. (/gateways/$ID)
+        If that fails the current transmission will not be started. The readings remain thus in
+        the list of their respective sensor.
+
+        Payload has the following format:
+        {
+            "measurements" : [
+
+
+            ]
+        }
         """
-        print("Begin Transmission.")
-        for sensor in self.sensors:
-            # Pack all measurements together.
-            payload = {
+        print("Probe Connection")
+
+        if self.probe():
+            print("Begin Transmission.")
+
+            # First try to transmit previously failed readings again.
+            for reading in self.readingsFailed[:]:
+                payload = {
                     'measurements' : []
-                    }
+                }
 
-            # Payload:
-            # "measurements" : [
-            #     [ "20150101",  ],
-            #     [],
-            # ]
+                payload_partial = self.preparePayload(reading)
+                payload['measurements'].append(payload_partial)
 
-            counter = 5
-            while not sensor.readings.empty() and counter >= 0:
-                counter -= 1
-                reading = sensor.readings.get()
-                values = {
+                try:
+                    url = globals.server["host"] + "/gateways/{}/sensors/{}/measurements/".format(self.id, reading.sensor.id)
+                    r = requests.post(url, json=payload, verify=False)
+
+                    # Reaching this code means that the transmission was successful
+                    self.readingsSuccess.append(reading)
+                    self.readingsFailed.remove(reading)
+                except requests.exceptions.ConnectionError:
+                    pass
+
+            for sensor in self.sensors:
+                # Pack all measurements together.
+                payload = {
+                    'measurements' : []
+                }
+
+                readingCachedList = []
+
+                counter = 20
+                while not sensor.readings.empty() and counter >= 0:
+                    counter -= 1
+
+                    # Reading removed from list!
+                    reading = sensor.readings.get()
+                    readingCachedList.append(reading) # Store in temporary list.
+
+                    payload_partial = self.preparePayload(reading)
+                    payload['measurements'].extend(payload_partial)
+
+                try:
+                    url = globals.server["host"] + "/gateways/{}/sensors/{}/measurements/".format(self.id, sensor.id)
+                    #print(payload)
+
+
+                    # pp = pprint.PrettyPrinter(indent=4)
+                    # pp.pprint(payload)
+
+                    r = requests.post(url, json=payload, verify=False)
+
+                    # Reaching this code means that the transmission was successful
+
+                    for reading in readingCachedList:
+                        # Move readings to success list.
+                        self.readingsSuccess.append(reading)
+                    readingCachedList.clear()
+                except requests.exceptions.ConnectionError:
+                    # Despite the check at the beginning of the transmission, it is still possible that the request
+                    # fails.
+                    for reading in readingCachedList:
+                        # Move readings to failed list.
+                        self.readingsFailed.append(reading)
+                    readingCachedList.clear()
+                    sys.stderr.write('Failed to make Connection')
+            print("End Transmission.")
+
     def probe(self):
         """
             Connects to the server and returns a boolean indicating whether it was successful or not.
@@ -72,31 +140,36 @@ class Gateway:
         except requests.exceptions.ConnectionError:
             pass
         return success
+
+    def preparePayload(self, reading):
+        """
+
+            Args:
+                readings : List of readings.
+            Returns:
+                A string containing the JSON data to form the POST request.
+        """
+        payload = []
+        values = {
                     0 : reading.cap,
                     1 : reading.temp1,
                     2 : reading.temp2,
                     3 : reading.humidity
                 }
 
-                for type,value in values.items():
-                    readingPayload = {
-                        "timestamp" : reading.timestamp.isoformat(), #"2015-01-01T00:00:00"
-                        "sensor_id" : reading.sensor.id,
-                        "measurement_type" : type,
-                        "value" : float("{0:.5f}".format(value))
-                    }
-                    payload['measurements'].append(readingPayload)
+        for type,value in values.items():
+            readingPayload = {
+                "timestamp" : reading.timestamp.isoformat(), #"2015-01-01T00:00:00"
+                "sensor_id" : reading.sensor.id,
+                "measurement_type" : type,
+                "value" : float("{0:.5f}".format(value))
+            }
+            payload.append(readingPayload)
 
-            # pp = pprint.PrettyPrinter(indent=4)
-            # pp.pprint(payload)
+        return payload
 
-            url = globals.server["host"] + "/gateways/{}/sensors/{}/measurements/".format(self.id, sensor.id)
-            try:
-                r = requests.post(url, json=payload, verify=False)
-            except requests.exceptions.ConnectionError:
-                sys.stderr.write('Failed to make Connection')
-        print("End Transmission.")
-                
+
+
     def fetch_configuration(self):
         url = globals.server["host"] + "/gateways/{}/config".format(self.id)
         r = requests.get(url)
